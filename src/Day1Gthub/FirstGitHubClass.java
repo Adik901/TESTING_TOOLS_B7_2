06036bd5bd6aebf310458200c60b69b6c01e0643
Adik901 <adikdima29@gmail.com>
2023-02-02 10:40:57 -0500

    MY second comnit, no changes

--- src/Day1Gthub/FirstGitHubClass.java
+++ src/Day1Gthub/FirstGitHubClass.java
@@ -2,6 +2,7 @@ package Day1Gthub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println(" Hey Githab");
+        System.out.println(" Hey GitHub");
+        System.out.println("1+1");
     }
 }
